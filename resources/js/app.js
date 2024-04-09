2669e0ec2e13d621852dc6228a226c4eb69e95db
bernard chisumo <bchisumo74@yahoo.com>
2024-04-09 11:53:40 +0100

    Install Vue Router , Vitejs/plugin-vue  . Configure the Vue Router to Laravel Application

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+
+/**Load The Vue Files**/
+
+import { createApp } from 'vue'
+import Mainapp from "./components/Mainapp.vue";
+import router from "./Router/router.js";
+
+const app = createApp()
+
+app.component('mainapp', Mainapp);
+app.use(router)
+app.mount('#app')
